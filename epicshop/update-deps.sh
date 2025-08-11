4b8b11e3c137d1c487878d9958faf09ec6109cb0
Kent C. Dodds <me+github@kentcdodds.com>
2025-08-11 12:31:12 -0600

    update all deps

--- epicshop/update-deps.sh
+++ epicshop/update-deps.sh
@@ -1,8 +1,8 @@
-npx npm-check-updates --dep prod,dev --upgrade --workspaces --root
-cd epicshop && npx npm-check-updates --dep prod,dev --upgrade --root
-cd epic-me && npx npm-check-updates --dep prod,dev --upgrade --root
+npx npm-check-updates --dep prod,dev --upgrade --workspaces --root --reject zod
+cd epicshop && npx npm-check-updates --dep prod,dev --upgrade --root --reject zod
+cd epic-me && npx npm-check-updates --dep prod,dev --upgrade --root --reject zod
 cd ../..
-rm -rf node_modules package-lock.json ./epicshop/package-lock.json ./epicshop/node_modules ./exercises/**/node_modules ./oauth/package-lock.json ./oauth/node_modules
+rm -rf node_modules package-lock.json ./epicshop/package-lock.json ./epicshop/node_modules ./exercises/**/node_modules ./epicshop/epic-me/package-lock.json ./epicshop/epic-me/node_modules
 npm install
 npm run setup
 npm run typecheck
